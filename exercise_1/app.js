ca4ec6e1c90a6a381c52b524fb0fe5b089d8a561
Graeme Elliott <19738684+GraemeElliott@users.noreply.github.com>
2017-09-24 19:41:26 +0100

    For in loop added

--- exercise_1/app.js
+++ exercise_1/app.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-// Fetch the JSON data
+    
+// For Loop
 
-    fetch("./data/contacts.json") //is this the best way?
+   fetch("./data/contacts.json") //is this the best way?
         .then(function (response) {
         return response.json();
     }).then(function (json) {
         //console.log(json[0].id);
         var tableArray = [];
         
-        for(var i = 0; i < 5; i++) {
+        for(var i = 0; i < json.length; i++) {
         var tableData = '<tr><td>' + json[i].id + '</td>';
         tableData+= '<td>' + json[i].first_name+ '</td>';
         tableData+= '<td>' + json[i].last_name + '</td>';
@@ -19,5 +20,29 @@ $(document).ready(function () {
 $('table').append(tableArray.join(''));     
 
     });
+
+// For In
+
+
+/*
+    fetch("./data/contacts.json") //is this the best way?
+        .then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        console.log(json[0].id);
+        var tableArray = [];
+        for (var i in json) {
+            var tableData = "<tr><td>" + json[i].id + "</td>";
+            tableData+= '<td>' + json[i].first_name+ '</td>';
+            tableData+= '<td>' + json[i].last_name + '</td>';
+            tableData+= '<td>' + json[i].country + '</td></tr>';
+            
+            tableArray.push(tableData);
+        }
+        $('table').append(tableArray.join(''));
+        
+        
+    });
+*/
 
 });
